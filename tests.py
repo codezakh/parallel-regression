3c0da2509128e3e153ebf0a2a23032081cffd7ef
Zaid Khan <brain@zaidkhan.me>
2017-10-27 12:06:22 -0400

    Added test for gradient calculation.

--- tests.py
+++ tests.py
@@ -3,11 +3,22 @@ import unittest
 import sys
 
 import numpy as np
+from pyspark import SparkContext
 
 import ParallelRegression as PR
 
 
 class ParallelRegressionTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(ParallelRegressionTestCase, cls).setUpClass()
+        cls.sc = SparkContext(appName="ParallelRegressionTestCase")
+
+    @classmethod
+    def tearDownClass(cls):
+        super(ParallelRegressionTestCase, cls).tearDownClass()
+        cls.sc.stop()
+
     def test_predict(self):
         vec_a = np.ones((50,))
         vec_b = np.ones((50, 1))
@@ -24,3 +35,16 @@ class ParallelRegressionTestCase(unittest.TestCase):
         print('estimated gradient: {}'.format(estimated_gradient))
         for actual, expected in zip(local_gradient, estimated_gradient):
             self.assertLess(abs(actual-expected), 0.0001)
+
+
+    def test_gradient(self):
+        lam = 1.0
+        beta = np.array([np.sin(t) for t in range(50)])
+        data = PR.readData('small.test', self.sc)
+        actual_gradient = PR.gradient(data, beta, lam)
+        expected_gradient = PR.estimateGrad(
+            lambda beta: PR.F(data, beta, lam),
+            beta,
+            0.00001)
+        print(actual_gradient)
+        print(expected_gradient)
